d18ee23a9a74808dadbcfbe6258c5f43c644a435
Brad Van Skyhawk <brad@vanskyhawk.com>
2016-11-14 11:17:50 -0700

    updates

--- Pdf/Engine/WkHtmlToPdfEngine.php
+++ Pdf/Engine/WkHtmlToPdfEngine.php
@@ -9,40 +9,40 @@ class WkHtmlToPdfEngine extends AbstractPdfEngine {
  * @access protected
  * @var string
  */
-	protected $binary = '/usr/bin/wkhtmltopdf';
+    protected $binary = '/usr/bin/wkhtmltopdf';
 
 /**
  * Constructor
  *
  * @param $Pdf CakePdf instance
  */
-	public function __construct(CakePdf $Pdf) {
-		parent::__construct($Pdf);
-	}
+    public function __construct(CakePdf $Pdf) {
+        parent::__construct($Pdf);
+    }
 
 /**
  * Generates Pdf from html
  *
  * @return string raw pdf data
  */
-	public function output() {
+    public function output() {
 
-		$content = $this->_exec($this->_getCommand(), $this->_Pdf->html());
+        $content = $this->_exec($this->_getCommand(), $this->_Pdf->html());
 
-		if (strpos(mb_strtolower($content['stderr']), 'error')) {
-			throw new CakeException("System error <pre>" . $content['stderr'] . "</pre>");
-		}
+        if (strpos(mb_strtolower($content['stderr']), 'error')) {
+            throw new CakeException("System error <pre>" . $content['stderr'] . "</pre>");
+        }
 
-		if (mb_strlen($content['stdout'], $this->_Pdf->encoding()) === 0) {
-			throw new CakeException("WKHTMLTOPDF didn't return any data");
-		}
+        if (mb_strlen($content['stdout'], $this->_Pdf->encoding()) === 0) {
+            throw new CakeException("WKHTMLTOPDF didn't return any data");
+        }
 
-		if ((int)$content['return'] !== 0 && !empty($content['stderr'])) {
-			throw new CakeException("Shell error, return code: " . (int)$content['return']);
-		}
+        if ((int)$content['return'] !== 0 && !empty($content['stderr'])) {
+            throw new CakeException("Shell error, return code: " . (int)$content['return']);
+        }
 
-		return $content['stdout'];
-	}
+        return $content['stdout'];
+    }
 
 /**
  * Execute the WkHtmlToPdf commands for rendering pdfs
@@ -51,72 +51,72 @@ class WkHtmlToPdfEngine extends AbstractPdfEngine {
  * @param string $input
  * @return string the result of running the command to generate the pdf
  */
-	protected function _exec($cmd, $input) {
-		$result = array('stdout' => '', 'stderr' => '', 'return' => '');
+    protected function _exec($cmd, $input) {
+        $result = array('stdout' => '', 'stderr' => '', 'return' => '');
 
-		$proc = proc_open($cmd, array(0 => array('pipe', 'r'), 1 => array('pipe', 'w'), 2 => array('pipe', 'w')), $pipes);
-		fwrite($pipes[0], $input);
-		fclose($pipes[0]);
+        $proc = proc_open($cmd, array(0 => array('pipe', 'r'), 1 => array('pipe', 'w'), 2 => array('pipe', 'w')), $pipes);
+        fwrite($pipes[0], $input);
+        fclose($pipes[0]);
 
-		$result['stdout'] = stream_get_contents($pipes[1]);
-		fclose($pipes[1]);
+        $result['stdout'] = stream_get_contents($pipes[1]);
+        fclose($pipes[1]);
 
-		$result['stderr'] = stream_get_contents($pipes[2]);
-		fclose($pipes[2]);
+        $result['stderr'] = stream_get_contents($pipes[2]);
+        fclose($pipes[2]);
 
-		$result['return'] = proc_close($proc);
+        $result['return'] = proc_close($proc);
 
-		return $result;
-	}
+        return $result;
+    }
 
 /**
  * Get the command to render a pdf
  *
  * @return string the command for generating the pdf
  */
-	protected function _getCommand() {
-		$binary = $this->config('binary');
+    protected function _getCommand() {
+        $binary = $this->config('binary');
 
-		if ($binary) {
-			$this->binary = $binary;
-		}
-		if (!is_executable($this->binary)) {
-			throw new CakeException(sprintf('wkhtmltopdf binary is not found or not executable: %s', $this->binary));
-		}
+        if ($binary) {
+            $this->binary = $binary;
+        }
+        if (!is_executable($this->binary)) {
+            throw new CakeException(sprintf('wkhtmltopdf binary is not found or not executable: %s', $this->binary));
+        }
 
-		$options = array(
-			'quiet' => true,
-			'print-media-type' => true,
-			'orientation' => $this->_Pdf->orientation(),
-			'page-size' => $this->_Pdf->pageSize(),
-			'encoding' => $this->_Pdf->encoding(),
-			'title' => $this->_Pdf->title()
-		);
+        $options = array(
+            'quiet' => true,
+            'print-media-type' => true,
+            'orientation' => $this->_Pdf->orientation(),
+            'page-size' => $this->_Pdf->pageSize(),
+            'encoding' => $this->_Pdf->encoding(),
+            'title' => $this->_Pdf->title()
+        );
 
-		$margin = $this->_Pdf->margin();
-		foreach ($margin as $key => $value) {
-			if ($value !== null) {
-				$options['margin-' . $key] = $value . 'mm';
-			}
-		}
-		$options = array_merge($options, (array)$this->config('options'));
+        $margin = $this->_Pdf->margin();
+        foreach ($margin as $key => $value) {
+            if ($value !== null) {
+                $options['margin-' . $key] = $value . 'mm';
+            }
+        }
+        $options = array_merge($options, (array)$this->config('options'));
 
-		$command = $this->binary;
-		foreach ($options as $key => $value) {
-			if (empty($value)) {
-				continue;
+        $command = $this->binary;
+        foreach ($options as $key => $value) {
+            if (empty($value)) {
+                continue;
             } elseif ($key === 'toc') {
                 $command .= ' toc';
-			} elseif ($value === true) {
-				$command .= ' --' . $key;
-			} elseif ($key === 'cover') {
-				$command .= sprintf(' %s %s', $key, escapeshellarg($value));
+            } elseif ($value === true) {
+                $command .= ' --' . $key;
+            } elseif ($key === 'cover') {
+                $command .= sprintf(' %s %s', $key, escapeshellarg($value));
             } else {
                 $command .= sprintf(' --%s %s', $key, escapeshellarg($value));
-			}
-		}
-		$command .= " - -";
+            }
+        }
+        $command .= " - -";
 
-		return $command;
-	}
+        return $command;
+    }
 }
